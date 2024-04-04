41225d71799254350da1c47172121de47f07da95
markyasser <82395903+markyasser@users.noreply.github.com>
2024-04-04 18:47:20 +0200

    modified build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-# Check if the correct number of arguments is provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <flex_file> <bison_file>"
+# Check if arguments are provided, if not, set default values
+if [ "$#" -eq 0 ]; then
+    flex_file=$(find ./Code/ -maxdepth 1 -type f -name "*.l" | head -n 1)
+    bison_file=$(find ./Code/ -maxdepth 1 -type f -name "*.y" | head -n 1)
+elif [ "$#" -eq 2 ]; then
+    flex_file="$1"
+    bison_file="$2"
+else
+    echo "Usage: $0 [<flex_file> <bison_file>]"
     exit 1
 fi
 
-flex_file="$1"
-bison_file="$2"
+# Check if files exist
+if [ ! -f "$flex_file" ]; then
+    echo "Flex file '$flex_file' not found."
+    exit 1
+fi
+
+if [ ! -f "$bison_file" ]; then
+    echo "Bison file '$bison_file' not found."
+    exit 1
+fi
 
 # Compile Flex file
 flex "$flex_file"
