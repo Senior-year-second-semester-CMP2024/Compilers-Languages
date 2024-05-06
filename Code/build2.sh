c1909b4d07d651254773ad1cb9e22fa59d5555d9
Karim Mahmoud <82693464+karimmahmoud22@users.noreply.github.com>
2024-05-06 19:19:08 +0200

    code works now

---
+++ Code/build2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# remove old output directory
+rm -r output
+
+# Create the output directory if it doesn't exist
+mkdir -p output
+
+touch output/symbol_table.txt
+touch output/errors.txt
+touch output/quadruples.txt
+
+# Compile the yacc and lex files
+bison -d spider.y
+flex spider.l
+
+# Move the generated files to the output directory
+mv spider.tab.c spider.tab.h lex.yy.c output/
+
+# Compile the generated C files
+gcc -g output/lex.yy.c output/spider.tab.c -o output/program -w
+
+# Execute the compiled program and redirect output to output.txt
+./output/program.exe < input.txt
+
+# Remove the input.txt file
+# rm input.txt
