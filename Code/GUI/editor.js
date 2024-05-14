bab45a4d44959f5c225ea8705fde06e8c0478fbd
markyasser <82395903+markyasser@users.noreply.github.com>
2024-05-14 22:56:33 +0300

    GUI enhancement

---
+++ Code/GUI/editor.js
@@ -0,0 +1,108 @@
+function duplicateLine(cm) {
+  var cursor = cm.getCursor();
+  var line = cm.getLine(cursor.line);
+  cm.replaceRange(line + "\n", { line: cursor.line, ch: 0 });
+  cm.setCursor({ line: cursor.line + 1, ch: cursor.ch });
+}
+
+var editor = CodeMirror.fromTextArea(document.getElementById("code-input"), {
+  lineNumbers: true,
+  mode: "text/x-csrc",
+  autoCloseBrackets: true,
+  autoCloseTags: true,
+  matchBrackets: true,
+  matchBrackets: true,
+  matchTags: { bothTags: true },
+  styleActiveLine: true,
+  smartIndent: true,
+  indentUnit: 4,
+  foldGutter: true,
+  gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+  extraKeys: {
+    "Ctrl-F": "findPersistent",
+    "Ctrl-H": "replace",
+    "Ctrl-J": "toMatchingTag",
+    "Ctrl-Q": function (cm) {
+      cm.foldCode(cm.getCursor());
+    },
+    "Ctrl-Alt-Down": function (cm) {
+      duplicateLine(cm);
+    },
+    Tab: function (cm) {
+      // Custom handling for Tab key
+      var cursor = cm.getCursor();
+      var line = cm.getLine(cursor.line);
+      var token = cm.getTokenAt(cursor);
+
+      // Check if cursor is inside or next to a word
+      if (token.type === "keyword" && token.string === "for") {
+        rep = "for (int i = 0; i < N; i++) {}";
+        cm.replaceRange(
+          rep,
+          { line: cursor.line, ch: token.start },
+          { line: cursor.line, ch: token.end }
+        );
+        cm.setCursor({
+          line: cursor.line,
+          ch: token.start + rep.length - 1,
+        });
+      } else if (token.type === "keyword" && token.string === "while") {
+        rep = "while (condition) {}";
+        cm.replaceRange(
+          rep,
+          { line: cursor.line, ch: token.start },
+          { line: cursor.line, ch: token.end }
+        );
+        cm.setCursor({ line: cursor.line, ch: token.start + rep.length - 1 });
+      } else if (token.type === "keyword" && token.string === "if") {
+        rep = "if (condition) {}";
+        cm.replaceRange(
+          rep,
+          { line: cursor.line, ch: token.start },
+          { line: cursor.line, ch: token.end }
+        );
+        cm.setCursor({ line: cursor.line, ch: token.start + rep.length - 1 });
+      } else if (token.type === "keyword" && token.string === "else") {
+        rep = "else {}";
+        cm.replaceRange(
+          rep,
+          { line: cursor.line, ch: token.start },
+          { line: cursor.line, ch: token.end }
+        );
+        cm.setCursor({ line: cursor.line, ch: token.start + rep.length - 1 });
+      } else if (token.type === "keyword" && token.string === "else if") {
+        rep = "else if (condition) {}";
+        cm.replaceRange(
+          rep,
+          { line: cursor.line, ch: token.start },
+          { line: cursor.line, ch: token.end }
+        );
+        cm.setCursor({ line: cursor.line, ch: token.start + rep.length - 1 });
+      } else if (token.type === "keyword" && token.string === "switch") {
+        rep = "switch (expression) {}";
+        cm.replaceRange(
+          rep,
+          { line: cursor.line, ch: token.start },
+          { line: cursor.line, ch: token.end }
+        );
+        cm.setCursor({ line: cursor.line, ch: token.start + rep.length - 1 });
+      } else if (token.type === "keyword" && token.string === "case") {
+        rep = "case value: break;";
+        cm.replaceRange(
+          rep,
+          { line: cursor.line, ch: token.start },
+          { line: cursor.line, ch: token.end }
+        );
+        cm.setCursor({ line: cursor.line, ch: token.start + rep.length - 1 });
+      } else if (token.type === "keyword" && token.string === "default") {
+        rep = "default: break;";
+        cm.replaceRange(
+          rep,
+          { line: cursor.line, ch: token.start },
+          { line: cursor.line, ch: token.end }
+        );
+        cm.setCursor({ line: cursor.line, ch: token.start + rep.length - 1 });
+      }
+    },
+  },
+});
--- Code/GUI/script.js
+++ Code/GUI/script.js
@@ -1,12 +1,14 @@
-var editor = CodeMirror.fromTextArea(document.getElementById("code-input"), {
-  lineNumbers: true,
-  mode: "text/x-csrc",
-});
-
 $(document).ready(function () {
   $("#compile-button").click(function (event) {
     event.preventDefault();
-    var code = $("#code-input").val();
+    var button = $(this);
+    var spinner = button.find(".spinner");
+    var buttonText = button.find(".button-text");
+
+    // Show spinner and hide button text
+    spinner.show();
+    buttonText.css("opacity", 0);
+    console.log("Compiling code...");
     $.ajax({
       url: "http://127.0.0.1:5000/compile",
       type: "POST",
@@ -14,13 +16,21 @@ $(document).ready(function () {
       data: JSON.stringify({ code: editor.getValue() }),
       success: function (response) {
         // Handle successful response
-        console.log("Compilation successful:", response);
-        // output = response.json();
-        writeToHTML(response);
+        // sleep for 1 second
+        setTimeout(() => {
+          console.log("Compilation successful:", response);
+          // output = response.json();
+          writeToHTML(response);
+        }, 10000);
       },
       error: function (xhr, status, error) {
         // Handle error
         console.error("Error:", status, error);
+      },
+      complete: function () {
+        // Hide spinner and show button text
+        spinner.hide();
+        buttonText.css("opacity", 1);
       },
     });
   });
@@ -39,6 +49,7 @@ function writeToHTML(output) {
   var quadruplesLines = output.quadruples;
   var quadruplesHTML = "";
   for (var j = 0; j < quadruplesLines.length; j++) {
+    // quadruplesHTML += quadruplesLines[j] + "<br>";
     quadruplesHTML += quadruplesLines[j] + "<br>";
   }
   document.getElementById("quadruples-output").innerHTML = quadruplesHTML;
